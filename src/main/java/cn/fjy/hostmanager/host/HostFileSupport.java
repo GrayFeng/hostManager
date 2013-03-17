d1e71fc5246115ba28d94d41ad7493e8bb65242f
GrayF <GrayF@PC>
2013-03-17 22:46:44 +0800

    host file support

---
+++ src/main/java/cn/fjy/hostmanager/host/HostFileSupport.java
@@ -0,0 +1,14 @@
+package cn.fjy.hostmanager.host;
+
+/**
+ * Description: HostFileSupport.java
+ * All Rights Reserved.
+ * @version 1.0  2013-3-18 下午5:42:17  
+ * @author Gray(jy.feng@zuche.com) 
+ */
+
+public class HostFileSupport {
+	
+	private String hostFileName = "hosts";
+
+}
