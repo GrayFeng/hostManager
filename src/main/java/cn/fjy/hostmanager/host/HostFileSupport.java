14cf7cd93d08bc58e586971e0cf28ce6c0b9b6d2
JY.Feng <tyfjy823@gmail.com>
2013-03-19 18:08:02 +0800

    add mapping handle

--- src/main/java/cn/fjy/hostmanager/host/HostFileSupport.java
+++ src/main/java/cn/fjy/hostmanager/host/HostFileSupport.java
@@ -1,4 +1,13 @@
 package cn.fjy.hostmanager.host;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import cn.fjy.hostmanager.pojo.Domain;
+
+import com.google.common.base.Strings;
+import com.google.common.io.Files;
 
 /**
  * Description: HostFileSupport.java
@@ -12,5 +21,137 @@ public class HostFileSupport {
 	private String hostFileName = "hosts";
 	
 	private String hostFilePath = "C://Windows//System32//drivers//etc";
+	
+	private String backupSuffix = ".hfm.bak";
+	
+	private String dividingLineStart = "#####hfm_start#####";
+	
+	private String dividingLineEnd = "#####hfm_end#####";
+	
+	/**
+	 * Backup current hostfile
+	 */
+	public void backup() {
+		File hostFile = getHostFile();
+		File backupFile = getBackupFile();
+		if (hostFile.exists()) {
+			if (backupFile.exists()) {
+				backupFile.delete();
+			}
+			try {
+				Files.copy(hostFile, backupFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Create hostfile from backupfile
+	 */
+	public void recover(){
+		File hostFile = getHostFile();
+		File backupFile = getBackupFile();
+		if (backupFile.exists()) {
+			if (hostFile.exists()) {
+				hostFile.delete();
+			}
+			try {
+				Files.copy(backupFile, hostFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * add domain mapping
+	 * @param domainList
+	 */
+	public void addDomainMapping(List<Domain> domainList){
+		String mappingStr = getDomainMappingStr(domainList);
+		if(!Strings.isNullOrEmpty(mappingStr)){
+			File hostFile = getHostFile();
+			if(hostFile.exists()){
+				try {
+					String context = getHostFileContextWithOutMapping();
+					if(context != null){
+						Files.write((context + mappingStr).getBytes(),hostFile);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	private String getHostFileContextWithOutMapping(){
+		StringBuffer buffer = null;
+		File hostFile = getHostFile();
+		if(hostFile.exists()){
+			try {
+				List<String> list = Files.readLines(hostFile, Charset.defaultCharset());
+				if(list != null && list.size() > 0){
+					buffer = new StringBuffer();
+					int startIndex = 0;
+					int endIndex = 0;
+					for(int i = 0 ; i < list.size() ; i++){
+						String str = list.get(i);
+						if(dividingLineStart.equals(str)){
+							startIndex = i;
+						}
+						if(dividingLineEnd.equals(str)){
+							endIndex = i;
+						}
+					}
+					if(startIndex != endIndex && startIndex < endIndex && endIndex < list.size()){
+						for(int i = 0;i<list.size();i++){
+							if(i < startIndex || i > endIndex){
+								buffer.append(list.get(i));
+								buffer.append("\r");
+								buffer.append("\n");
+							}
+						}
+					}else{
+						for(String str : list){
+							buffer.append(str);
+						}
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return buffer == null ? null : buffer.toString();
+	} 
+	
+	
+	private String getDomainMappingStr(List<Domain> domainList){
+		StringBuffer buffer  = null;
+		if(domainList != null && domainList.size() > 0){
+			buffer = new StringBuffer();
+			buffer.append(dividingLineStart);
+			buffer.append("\r");
+			buffer.append("\n");
+			for(Domain domain : domainList){
+				buffer.append(domain.getIp() + "    " + domain.getDomain());
+			}
+			buffer.append(dividingLineEnd);
+			buffer.append("\r");
+			buffer.append("\n");
+		}
+		return buffer == null ? null :buffer.toString();
+	}
+	
+	private File getHostFile(){
+		return new File(hostFilePath + File.separator + hostFileName);
+	}
+	
+	private File getBackupFile(){
+		return new File(getHostFile().getAbsoluteFile() + backupSuffix);
+	}
+	
+	public static void main(String[] args) {
+	}
 
 }
